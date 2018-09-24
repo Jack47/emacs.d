4992d27be47a9f085f993dd0786428d204c9a6ed
Jack47 <csdvpn@gmail.com>
2018-09-24 10:25:48 +0800

    rebase from upstream/master

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,18 +1,16 @@
-<<<<<<< HEAD
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
 
   (when (maybe-require-package 'flycheck-color-mode-line)
     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
-=======
 ;; Override default flycheck triggers
-(setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
-      flycheck-idle-change-delay 0.8)
+;(setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+;      flycheck-idle-change-delay 0.8)
 
-(setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+;(setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
 
->>>>>>> support jsx flycheck
+;>>>>>>> support jsx flycheck
 
 ;; http://www.flycheck.org/manual/latest/index.html
 (require 'flycheck)
